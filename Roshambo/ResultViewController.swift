2cdc434e055386f9a75a404b034920a66808700c
Andreas Talg <ATalg@Andreass-MacBook-Air.local>
2015-04-16 19:20:10 +0200

    Graphical design ready, without code and connections

---
+++ Roshambo/ResultViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ResultViewController.swift
+//  Roshambo
+//
+//  Created by Andreas Talg on 16.04.15.
+//  Copyright (c) 2015 Andreas Talg. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ResultViewController: UIViewController {
+    
+}
